1a8c5263d7f8714a31e4672032b5cfd7d0260dec
Kyle <kylezhengkz@gmail.com>
2025-05-06 10:55:20 -0400

    updated like unlike

--- client/src/pages/PostLikeSection.jsx
+++ client/src/pages/PostLikeSection.jsx
@@ -5,13 +5,22 @@ import { api } from '../api/api.js'
 
 function PostLikeSection({ postId }) {
   const[likes, setLikes] = useState([])
-  const[toggleLike, setToggleLike] = useState(false)
+  const[didLike, setDidLike] = useState()
 
   useEffect(() => {
     async function fetchLikes() {
       let res = await api.get(`/post/${postId}/getLikes`)
       console.log(res.data)
       setLikes(res.data)
+
+      let res_1 = await api.get(`/post/${postId}/didUserLikePost`)
+      let didUserLikePost = res_1.data
+      console.log(didUserLikePost)
+      if (didUserLikePost) {
+        setDidLike(true)
+      } else {
+        setDidLike(false)
+      }
     }
     fetchLikes()
   }, [])
@@ -20,11 +29,15 @@ function PostLikeSection({ postId }) {
     console.log(likes)
   }, [likes])
   
-  function likePost() {
-    if (toggleLike) {
-      setToggleLike(false)
+  async function likePost() {
+    if (didLike) {
+      let res = await api.post(`/post/${postId}/unlikePost`)
+      console.log(res.data)
+      setDidLike(false)
     } else {
-      setToggleLike(true)
+      let res = await api.post(`/post/${postId}/likePost`)
+      console.log(res.data)
+      setDidLike(true)
     }
   }
 
@@ -33,8 +46,8 @@ function PostLikeSection({ postId }) {
     <div className="my_border">
       {likes.length === 0 && <p> No likes </p>}
       {likes.length > 0 && <p>{likes.length} Likes</p>}
-      {!toggleLike && <button onClick={() => likePost()}>Like post</button>}
-      {toggleLike && <button onClick={() => likePost()}>Unlike post</button>}
+      {!didLike && <button onClick={() => likePost()}>Like post</button>}
+      {didLike && <button onClick={() => likePost()}>Unlike post</button>}
     </div>
   )
 }
